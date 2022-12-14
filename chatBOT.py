47b56591922131409da9a31b42672023d2010d24
Internee <Internee@LRO-SH-21.vu.edu.pk>
2022-12-14 08:59:29 +0500

    Hi this is just the very first version of chatterbot implementation

---
+++ chatBOT.py
@@ -0,0 +1,11 @@
+#import libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly as pt
+import streamlit as st
+import sklearn as sk
+#Created By Izhar Khan Khattak#
+st.title("Izhar Khan Khattak")
+st.header("Hi! we are gonna create ChatterBOT!")
