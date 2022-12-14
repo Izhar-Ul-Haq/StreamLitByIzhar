0c054cfc531a8bfd83ee968916c38f30c48fb791
Internee <Internee@LRO-SH-21.vu.edu.pk>
2022-12-14 09:39:05 +0500

    video and image added

--- chatBOT.py
+++ chatBOT.py
@@ -9,3 +9,6 @@ import sklearn as sk
 #Created By Izhar Khan Khattak#
 st.title("Izhar Khan Khattak")
 st.header("Hi! we are gonna create ChatterBOT!")
+st.video("https://youtu.be/yhQG8uIolX4")
+st.image("https://media.licdn.com/dms/image/C4D03AQHMXbf3SYxLaw/profile-displayphoto-shrink_800_800/0/1647682327612?e=1676505600&v=beta&t=jPBfDVRbVkK1fGaSzq9j0CyZixmcFP_3AHjyQcRx9X4")
+st.camera_input("Take a picture")
